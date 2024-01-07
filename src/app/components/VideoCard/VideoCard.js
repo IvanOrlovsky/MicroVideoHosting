f6b06e65c4d49169767676f0bb89c02168037177
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-01-07 15:53:23 +0300

    made working home page

--- src/app/components/VideoCard/VideoCard.js
+++ src/app/components/VideoCard/VideoCard.js
@@ -2,7 +2,14 @@ import styles from './videocard.module.scss'
 import Link from 'next/link';
 
 export default function VideoCard(props) {
-    const { smt } = props;
+    const { video_id, title, description } = props;
 
-    return <Link href={''}>{tag}</Link>
+    return (
+    <Link href={`/watch/${video_id}`}>
+      <div className={`p-6 rounded-lg bg-slate-500 shadow-md`}>
+        <h2 className="text-xl font-semibold mb-2 text-white border-b-2">{title}</h2>
+        <p className="text-gray-200">{description}</p>
+      </div>
+    </Link>
+    )
 }
--- src/app/components/VideoGrid/VideoGrid.js
+++ src/app/components/VideoGrid/VideoGrid.js
@@ -1,16 +1,28 @@
-import styles from './videogrid.module.scss'
+'use client';
+
+import styles from './videogrid.module.scss';
+import { useState, useEffect } from 'react';
+import VideoCard from '../VideoCard/VideoCard';
 
 export default function VideoGrid() {
     
+    const [videos, setVideos] = useState([]);
 
+    const fetchVideos = async () => {
+        const response = await fetch('/api/videos', { method:  'GET' });  
+        const videos = await response.json();
+        setVideos(videos);
+    }
+
+    useEffect(() => {
+        fetchVideos();
+    }, []);
 
     return (
         <div className={styles.video_grid}>
-            <span className='bg-red-500 rounded-lg text-white'>dada</span>
-            <span className='bg-red-500 rounded-lg text-white'>dada</span>
-            <span className='bg-red-500 rounded-lg text-white'>dada</span>
-            <span className='bg-red-500 rounded-lg text-white'>dada</span>
-            <span className='bg-red-500 rounded-lg text-white'>dada</span>
+            {videos.map((video_info) => (
+                <VideoCard key={video_info.video_id} video_id={video_info.video_id} title={video_info.title} description={video_info.description} />
+            ))}
         </div>
-    )
+    );
 }
