bb1843224053bd874760e7656bd4989b0a5cf68b
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-01-08 16:45:06 +0300

    corrected main page videos api route 3

--- src/app/api/videos/route.js
+++ src/app/api/videos/route.js
@@ -1,7 +1,7 @@
 import { sql } from '@vercel/postgres';
 export const fetchCache = 'force-no-store';
 export async function GET(request) {
-    const id = Math.floor(Math.random() * 2 - 2);
+    const id = Math.floor(Math.random() * 2 - 100);
     const { rows } = await sql`SELECT * FROM video WHERE video_id > ${id};`;
     
     const data = JSON.stringify(rows);
